c3c0b1d7d5231f85550d2ddfd4547aa71178bb94
Curtis Mason <cumason@bu.edu>
2019-03-16 20:30:44 -0400

    Added authorization function: validate_access_token

--- holdette-api/HoldetteCloudServices/UserPools/__init__.py
+++ holdette-api/HoldetteCloudServices/UserPools/__init__.py
@@ -4,8 +4,13 @@ import hmac
 import hashlib
 import base64
 import uuid
+import urllib.request as req
+import json
+from jwt.algorithms import RSAAlgorithm
+import jwt
+from jose import jwt as jt, jwk as jk
+import re
 from .Errors import Error
-
 class UserPoolAPI():
 	error = Error()
 	def __init__(self, pool_dict, iam_dict):
@@ -14,11 +19,17 @@ class UserPoolAPI():
 			self.client_secret = pool_dict['app-client']['secret']
 			self.pool_id = pool_dict['pool-id']
 			self.required_attributes = pool_dict['pool-required-attributes']
+			self.pool_region = pool_dict['pool-region']
+			self.keys_url = 'https://cognito-idp.{0}.amazonaws.com/{1}/.well-known/jwks.json'.format(self.pool_region, self.pool_id)
 			self.client = boto3.client('cognito-idp', 
 				aws_access_key_id=iam_dict['access-key'], 
 				aws_secret_access_key=iam_dict['access-secret'],
-				region_name='us-east-1'
+				region_name=self.pool_region
 				)
+			# https://cognito-idp.{region}.amazonaws.com/{userPoolId}/.well-known/jwks.json
+			response = req.urlopen(self.keys_url)
+			self.keys = json.loads(response.read())['keys']
+			# Assumption, encryption kids don't change therefore its cached
 		except ValueError as e:
 			print(e)
 			raise ValueError('Check fields in configuration file')
@@ -90,8 +101,44 @@ class UserPoolAPI():
 		except Exception as e:
 			print(e)
 			return self.error.ERROR, "Internal Server Error"
-			
+		print(resp)
 		return resp, None
 
+	def validate_access_token(self, bearer_token):
+		"""
+		@returns: (Error, string) where upon success the first value will be the decrypted
+			token, otherwise it will be an Error value. String will be errmsg if the first value is Error, and None otherwise.
+		"""
+
+		# TODO Timezone auth expiration issues?
+		try:
+			headers = jt.get_unverified_headers(bearer_token)
+
+			key_index = -1
+			print(headers)
+			kid = headers['kid']
+			for i in range(len(self.keys)):
+				if kid == self.keys[i]['kid']:
+					key_index = i
+					break
+
+			if key_index == -1:
+				return self.error.INVALID_KID, 'Invalid Token. Unknown kid id.'
+
+			jwkValue = self.keys[key_index]
+
+			publicKey = RSAAlgorithm.from_jwk(json.dumps(jwkValue))
+			decoded_token = jwt.decode(bearer_token, publicKey, algorithm=jwkValue['alg'])
+			return decoded_token, None
+
+		except jwt.exceptions.ExpiredSignatureError as e:
+			return self.error.EXPIRED_TOKEN, 'Expired token. Please refresh session.'
+
+		except Exception as e:
+			print(e)
+			return self.error.CORRUPTED_TOKEN, 'Corrupted Token or internal server error.'
+
+		
+		
 
 
